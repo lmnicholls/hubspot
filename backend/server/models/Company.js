334747db06cdcc5ef576c0cba299060e3df24598
dalva19 <75229602+dalva19@users.noreply.github.com>
2021-08-13 13:57:59 -0400

    CompanySchema in models folder

---
+++ backend/server/models/Company.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const CompanySchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  companyOwner: { type: String, required: true },
+  phoneNumber: { type: String, required: true },
+  city: { type: String, required: true },
+  state: { type: String, required: true },
+  postalCode: { type: String, required: true },
+  logo: { type: String },
+  deals: { type: Schema.Types.ObjectId, ref: "Deal" },
+});
+
+module.exports = mongoose.model("Company", CompanySchema);
