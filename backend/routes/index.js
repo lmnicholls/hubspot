643dc1ee5d2a66a83b4e1265e56f31aa40c6f9a5
dalva19 <75229602+dalva19@users.noreply.github.com>
2021-08-13 13:23:03 -0400

    server db connection and tester GET route on companies

---
+++ backend/routes/index.js
@@ -0,0 +1,8 @@
+const router = require("express").Router();
+const companyRoutes = require("./companies");
+const dealsRoutes = require("./deals");
+
+router.use("/companies", companyRoutes);
+// router.use("/deals", dealsRoutes);
+
+module.exports = router;
--- backend/server/index.js
+++ backend/server/index.js
@@ -1,17 +1,31 @@
 const path = require("path");
 const express = require("express");
-
-const PORT = process.env.PORT || 3001;
-
+const mongoose = require("mongoose");
+const cors = require("cors");
+const PORT = process.env.PORT || 8000;
 const app = express();
+const routes = require("../routes/index");
+
+//local connection to start for dev and testing
+//plan for deployment to heroku
+mongoose.connect(
+  "mongodb://localhost/closing-time",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  () => console.log("connected to db")
+);
+
+//middlewares
+app.use(express.json()); //body parser
+app.use(cors());
+
+//routes
+app.use(routes);
 
 // Have Node serve the files for our built React app
 app.use(express.static(path.resolve(__dirname, "../../client/build")));
-
-// Handle GET requests to /api route
-app.get("/api", (req, res) => {
-  res.json({ message: "Hello from server!" });
-});
 
 // All other GET requests not handled before will return our React app
 app.get("*", (req, res) => {
