fcf89ed865e05b1775d1535cab82b9ba3066f2ed
dalva19 <75229602+dalva19@users.noreply.github.com>
2021-08-13 14:25:24 -0400

    POST /companies adds new company

--- backend/server/routes/companies.js
+++ backend/server/routes/companies.js
@@ -4,4 +4,26 @@ router.get("/", (req, res) => {
   res.status(200).send("hello from the companies");
 });
 
+router.post("/", async (req, res) => {
+  //needs validation
+  const newCompany = new Company({
+    name: req.body.name,
+    companyOwner: req.body.companyOwner,
+    phoneNumber: req.body.phoneNumber,
+    city: req.body.city,
+    state: req.body.state,
+    postalCode: req.body.postalCode,
+    logo: req.body.logo,
+  });
+
+  try {
+    await newCompany.save();
+    //for testing puposes get back company._id
+    //does frontend need anything back when saving company?
+    res.status(200).send({ newCompany: newCompany._id });
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
+
 module.exports = router;
