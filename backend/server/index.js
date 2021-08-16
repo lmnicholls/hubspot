eb137dba893b53d9b8efb067486a93e053d5e0ba
dalva19 <75229602+dalva19@users.noreply.github.com>
2021-08-16 07:57:36 -0400

    configering client routes

--- backend/server/index.js
+++ backend/server/index.js
@@ -27,13 +27,24 @@ app.use(cors());
 //routes
 app.use(routes);
 
-// Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, "../../client/build")));
+//cofiguring client routes
+if (process.env.NODE_ENV === "production") {
+  //aaron's code from the lesson
+  //app.use(express.static("../../client/build"));
+  // const path = require("path");
+  // app.get("*", (req, res) => {
+  //   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  // });
 
-// All other GET requests not handled before will return our React app
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../../client/build", "index.html"));
-});
+  //code that lauren set up website
+  // Have Node serve the files for our built React app
+  app.use(express.static(path.resolve(__dirname, "../../client/build")));
+
+  // All other GET requests not handled before will return our React app
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../../client/build", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
