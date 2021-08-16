2a3c6b6128b90c20d7a898518f8c460a583aa7b4
Jeremy Wheless <whelessjw@gmail.com>
2021-08-16 10:35:13 -0400

    hooked up get action to backend

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,11 +1,12 @@
-import testData from "./testData.json";
+import axios from "axios";
+
 import { GET_COMPANIES } from "./names";
 
-export const getCompanies = () => {
-  const companyTestData = testData;
+export const getCompanies = async () => {
+  const request = await axios.get("http://localhost:8000/companies");
 
   return {
     type: GET_COMPANIES,
-    payload: companyTestData,
+    payload: request,
   };
 };
--- client/src/reducers/companiesReducer.js
+++ client/src/reducers/companiesReducer.js
@@ -3,7 +3,7 @@ import { GET_COMPANIES } from "../actions/names";
 export const companiesReducer = function (state = null, action) {
   switch (action.type) {
     case GET_COMPANIES:
-      return action.payload;
+      return action.payload.data;
 
     default:
       return state;
