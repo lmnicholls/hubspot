e97d409992fadc2efd6633f741d94334a61319f8
Jeremy Wheless <whelessjw@gmail.com>
2021-08-17 08:07:52 -0400

    create edit company action

--- client/src/actions/names.js
+++ client/src/actions/names.js
@@ -1,3 +1,4 @@
 export const GET_COMPANIES = "GET_COMPANIES";
 export const GET_ONE_COMPANY = "GET_ONE_COMPANY";
 export const ADD_COMPANY = "ADD_COMPANY";
+export const EDIT_COMPANY = "EDIT_COMPANY";
