c8b482a0e45dad6d12b1bd659a17845d03fbb9ec
Jeremy Wheless <whelessjw@gmail.com>
2021-08-17 08:16:37 -0400

    add EDIT_COMPANY case to reducer

--- client/src/reducers/companiesReducer.js
+++ client/src/reducers/companiesReducer.js
@@ -1,12 +1,26 @@
-import { ADD_COMPANY, GET_COMPANIES } from "../actions/names";
+import { ADD_COMPANY, GET_COMPANIES, EDIT_COMPANY } from "../actions/names";
 
 export const companiesReducer = function (state = null, action) {
   switch (action.type) {
     case GET_COMPANIES:
       return action.payload.data;
+
     case ADD_COMPANY:
       //add the new company to the end of the array
       return [...state, action.payload.data];
+
+    case EDIT_COMPANY:
+      const updatedCompany = action.payload.data;
+
+      const newCompaniesList = state.map((company) => {
+        if (company._id === updatedCompany._id) {
+          return updatedCompany;
+        } else {
+          return company;
+        }
+      });
+      return newCompaniesList;
+
     default:
       return state;
   }
