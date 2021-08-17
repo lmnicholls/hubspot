9a28afa3fc60f9cac4ce7c27b33beb79bc3642a0
Lauren Nicholls <lauren.nicholls1@gmail.com>
2021-08-17 09:49:01 -0400

    added nav styling to deals container

--- client/src/components/deals/DealsContainer.js
+++ client/src/components/deals/DealsContainer.js
@@ -1,13 +1,38 @@
 import React from "react";
 import NavigationTabs from "../Navigation/NavigationTabs";
 import DealsDragAndDrop from "./DealsDragAndDrop";
+import { Button } from "react-bootstrap";
+import { StyleSheet, css } from "aphrodite";
 
 export default function DealsContainer() {
   return (
     <>
-      <NavigationTabs defaultActiveKey="/companies" />
-      <button>Add Deal</button>
+      <div className={css(styles.nav_buttons)}>
+        <NavigationTabs defaultActiveKey="/companies" />
+        <Button className={css(styles.addDealButton)}>Add Deal</Button>
+      </div>
       <DealsDragAndDrop />
     </>
   );
 }
+
+const styles = StyleSheet.create({
+  addDealButton: {
+    backgroundColor: "#25537d !important",
+    fontFamily: "Quicksand ",
+    borderColor: "#25537d",
+    fontWeight: "bold",
+    color: "white",
+    ":hover": {
+      backgroundColor: "#193753",
+    },
+  },
+  nav_buttons: {
+    paddingLeft: "15px",
+    paddingRight: "15px",
+    display: "flex",
+    flexFlow: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+});
