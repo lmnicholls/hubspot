d50c8601afea47894861c0f7771aa8ef01347734
Lauren Nicholls <lauren.nicholls1@gmail.com>
2021-08-17 10:24:31 -0400

    deals drag and drop component

--- client/src/components/deals/DealsDragAndDrop.js
+++ client/src/components/deals/DealsDragAndDrop.js
@@ -1,12 +1,76 @@
 import React from "react";
+import { StyleSheet, css } from "aphrodite";
 
 export default function DealsDragAndDrop() {
   return (
     <>
-      <h1>
-        This is where the deals will be displayed with drag and drop
-        functionality
-      </h1>
+      <div className={css(styles.dealTable)}>
+        <div className={css(styles.tableHeaders)}>
+          <div className={css(styles.headers)}>INITIATED</div>
+          <div className={css(styles.headers)}>QUALIFIED</div>
+          <div className={css(styles.headers)}>CONTRACT SENT</div>
+          <div className={css(styles.headers)}>CLOSED WON</div>
+          <div className={css(styles.headers)}>CLOSED LOST</div>
+        </div>
+        <div className={css(styles.dealsRow)}>
+          <div className={css(styles.dealColumn)}></div>
+          <div className={css(styles.dealColumn)}></div>
+          <div className={css(styles.dealColumn)}></div>
+          <div className={css(styles.dealColumn)}></div>
+          <div className={css(styles.dealColumn)}></div>
+        </div>
+        <div className={css(styles.totalRow)}>
+          <div className={css(styles.total)}>Total: $</div>
+          <div className={css(styles.total)}>Total: $</div>
+          <div className={css(styles.total)}>Total: $</div>
+          <div className={css(styles.total)}>Total: $</div>
+          <div className={css(styles.total)}>Total: $</div>
+        </div>
+      </div>
     </>
   );
 }
+
+const styles = StyleSheet.create({
+  dealTable: {
+    fontFamily: "Quicksand !important",
+    borderBottom: "1px solid gray",
+    borderTop: "1px solid gray",
+    borderLeft: "1px solid gray",
+    marginRight: "20px",
+    marginLeft: "20px",
+  },
+  headers: {
+    textAlign: "center",
+    width: "20%",
+    borderRight: "1px solid gray",
+    borderBottom: "1px solid gray",
+  },
+  dealsRow: {
+    display: "flex",
+    flexFlow: "row",
+    borderBottom: "1px solid gray",
+  },
+  dealColumn: {
+    borderRight: "1px solid gray",
+    height: "200px", // min height for deals
+    width: "20%",
+    ":hover": {
+      backgroundColor: "rgba(0, 0, 0, 0.075)",
+    },
+  },
+  tableHeaders: {
+    display: "flex",
+    flexFlow: "row",
+    backgroundColor: "rgb(220, 220, 224)",
+  },
+  totalRow: {
+    display: "flex",
+    flexFlow: "row",
+  },
+  total: {
+    textAlign: "center",
+    borderRight: "1px solid gray",
+    width: "20%",
+  },
+});
