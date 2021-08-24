1a1c0a83b34dae45a7e0bc9cfffab3f1f8ac7bb1
Lauren Nicholls <lauren.nicholls1@gmail.com>
2021-08-24 15:36:28 -0400

    added dropdown for chart #2

--- client/src/components/dashboard/OpportunityForRevenue.jsx
+++ client/src/components/dashboard/OpportunityForRevenue.jsx
@@ -2,60 +2,132 @@ import React from "react";
 import { useSelector } from "react-redux";
 import Highcharts from "highcharts";
 import HighchartsReact from "highcharts-react-official";
-import cylinder from "highcharts/modules/cylinder";
-import highcharts3d from "highcharts/highcharts-3d";
+import HC_drilldown from "highcharts/modules/drilldown";
 
-export default function OpportunityForRevenue() {
-  let dealObject = {};
+export default function DealsClosedVsLost() {
+  HC_drilldown(Highcharts);
+
   const deals = useSelector((state) => state.deals);
-  const stages = ["INITIATED", "QUALIFIED", "CONTRACT SENT"];
 
-  deals
-    .filter((deal) => stages.includes(deal.stage.status.toUpperCase()))
-    .forEach((deal) => {
-      if (
-        dealObject[deal.stage.status.toUpperCase()] === undefined ||
-        dealObject[deal.stage.status.toUpperCase()] === 0
-      ) {
-        return (dealObject[deal.stage.status.toUpperCase()] = deal.amount);
-      } else {
-        return (dealObject[deal.stage.status.toUpperCase()] =
-          dealObject[deal.stage.status.toUpperCase()] + deal.amount);
-      }
-    });
+  const count = function (deals, classifier) {
+    classifier = classifier || String;
+    return deals.reduce(function (counter, deal) {
+      var p = classifier(deal);
+      counter[p] = counter.hasOwnProperty(p)
+        ? counter[p] + deal.amount
+        : deal.amount;
+      return counter;
+    }, {});
+  };
 
-  highcharts3d(Highcharts);
-  cylinder(Highcharts);
+  const dealStatus = function (deal) {
+    return deal.stage.status.toLowerCase();
+  };
+
+  const dealTypeAmount = count(deals, dealStatus);
+
+  const dealsInitiatedObject = {};
+  deals.forEach((deal) => {
+    if (deal.stage.status.toUpperCase() === "INITIATED") {
+      return (dealsInitiatedObject[deal.company.companyName] = deal.amount);
+    }
+  });
+
+  const dealsQualifiedObject = {};
+  deals.forEach((deal) => {
+    if (deal.stage.status.toUpperCase() === "QUALIFIED") {
+      return (dealsQualifiedObject[deal.company.companyName] = deal.amount);
+    }
+  });
+
+  const dealsContractSentObject = {};
+  deals.forEach((deal) => {
+    if (deal.stage.status.toUpperCase() === "CONTRACT SENT") {
+      return (dealsContractSentObject[deal.company.companyName] = deal.amount);
+    }
+  });
 
   const chartOptions = {
     chart: {
       type: "pie",
-      options3d: {
-        enabled: true,
-        alpha: 45,
-      },
     },
     title: {
+      verticalAlign: "top",
       text: "Opportunity for Revenue",
+    },
+    tooltip: {
+      pointFormat: "{series.name}: <b>{point.percentage:.1f}%</b>",
+    },
+    accessibility: {
+      point: {
+        valueSuffix: "%",
+      },
+    },
+    legend: {
+      layout: "vertical",
+      align: "left",
+      verticalAlign: "top",
+      floating: true,
+      borderWidth: 1,
+      backgroundColor: "#FFFFFF",
+      shadow: true,
+      itemMarginTop: 5,
+      itemMarginBottom: 5,
+      x: 0,
+      y: 60,
     },
     plotOptions: {
       pie: {
-        innerSize: 100,
-        depth: 45,
-      },
-      series: {
+        allowPointSelect: true,
+        cursor: "pointer",
         dataLabels: {
           enabled: true,
-          format: "{point.name} </br> $ {y}",
+          format: `{point.name}: $ {point.y}`,
         },
+        showInLegend: true,
       },
     },
     series: [
       {
-        name: "Potential Revenue:",
-        data: Object.entries(dealObject),
+        name: "Percentage",
+        data: [
+          {
+            y: dealTypeAmount["Initiated".toLowerCase()],
+            name: "Initiated",
+            drilldown: "initiated",
+          },
+          {
+            y: dealTypeAmount["Qualified".toLowerCase()],
+            name: "Qualified",
+            drilldown: "qualified",
+          },
+          {
+            y: dealTypeAmount["Contract Sent".toLowerCase()],
+            name: "Contract Sent",
+            drilldown: "contract sent",
+          },
+        ],
       },
     ],
+    drilldown: {
+      series: [
+        {
+          id: "initiated",
+          name: "Initiated",
+          data: Object.entries(dealsInitiatedObject),
+        },
+        {
+          id: "qualified",
+          name: "Qualified",
+          data: Object.entries(dealsQualifiedObject),
+        },
+        {
+          id: "contract sent",
+          name: "Contract Sent",
+          data: Object.entries(dealsContractSentObject),
+        },
+      ],
+    },
   };
 
   return (
